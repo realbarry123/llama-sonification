76f74a918918e8c42c266a408a673f66b7db69b8
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-03-10 09:22:31 -0400

    Clean up main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from sonification import sonify
 from data import normalize, pca_reduce
 from scipy.io import wavfile
 from plot import histogram
-# torch.set_num_threads(1)
+
+NEW_TOKENS = 12
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
 
-inputs = tokenizer("I am ", return_tensors="pt").to(model.device)
-# inputs = tokenizer("Question:\nWho are you?\nAnswer:\n", return_tensors="pt").to(model.device)
+inputs = tokenizer("I am", return_tensors="pt").to(model.device)
 
 with torch.no_grad():
     output = model.generate(
         **inputs,
-        max_new_tokens=32,
+        max_new_tokens=NEW_TOKENS,
         return_dict_in_generate=True,
         output_hidden_states=True,
         output_scores=False
@@ -26,12 +26,12 @@ generated_ids = output.sequences
 generated_text = tokenizer.convert_ids_to_tokens(generated_ids[0], skip_special_tokens=True)
 print(generated_text)
 
-hidden_states = output.hidden_states # (step, layers, batch, seq, hidden)
+hidden_states = output.hidden_states 
+# (step(tuple), layers(tuple), batch(tensor), seq(tensor), hidden(tensor))
 
+# Copy/reshape select hidden states to steps
 steps = []
-
 for step in hidden_states:
-
     layers = torch.stack(
         [layer.squeeze(0)[-1] for layer in step]
     )  # (layers, hidden)
@@ -40,15 +40,11 @@ for step in hidden_states:
 
 tensor = torch.stack(steps) # (steps, layers, hidden)
 
-# tensor = torch.diff(tensor, dim=0)
-
 time, layers, hidden = tensor.shape
 audio_tensor = tensor.reshape(time * layers, hidden).float() # (time, voices)
-audio_tensor = torch.diff(audio_tensor)
-audio_tensor = pca_reduce(audio_tensor, q=128)
+# audio_tensor = pca_reduce(audio_tensor, q=8)
 audio_tensor = normalize(audio_tensor, 50, 1050)
-#histogram(audio_tensor)
-#exit()
-wav = sonify(audio_tensor, 0.08, do_interpolate=False, do_stereo=True, do_diff=True).numpy()
-wavfile.write("03-02.6_diff-pca-q=128.wav", 44100, wav)
+
+wav = sonify(audio_tensor, 0.12, do_interpolate=False, do_stereo=True, do_diff=False).numpy()
+wavfile.write("03-09.0_demo.wav", 44100, wav)
 print("\a")
