9bf3f6554654a9f30a2fd5f81317e669d7acd759
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-03-21 20:33:22 -0400

    Fix commit error

--- data.py
+++ data.py
@@ -10,6 +10,8 @@ def pca_reduce(x, q):
     x = x - x.mean(dim=0, keepdim=True)
     U, S, V = torch.pca_lowrank(x, q=q)
     return U @ torch.diag(S)def to_uniform(x, lower, upper):
+
+def to_uniform(x, lower, upper):
     T, V = x.shape
     rankings = x.flatten().argsort().argsort().view(T, V)
     normalized = rankings / torch.max(rankings)
