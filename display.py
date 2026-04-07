c6f09dd375c8ccef1c174c26cbb25607e28ea697
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-04-06 21:34:30 -0400

    Update display.py

--- display.py
+++ display.py
@@ -5,12 +5,13 @@ from model_wrapper import ModelWrapper
 import threading
 import queue
 
+FULL_SCREEN = False
 
-# VARIABLES
+# MODEL SETUP
 
 sonifier = Sonifier((1, 17, 2048), 2/17, fs=44100)
 model = ModelWrapper()
-model.seed("I")
+model.seed("Hello, world")
 
 
 # PRODUCER
@@ -31,17 +32,22 @@ producer_thread.start()
 # GRAPHICS SETUP
 
 pygame.init()
-VW = 3024//2
-VH = 1964//2
+VW = 500
+VH = 500
+
+if FULL_SCREEN:
+    VW = 1512
+    VH = 982
 INIT_TEXT_COLOR = 255
 INIT_D_TEXT_COLOR = -5
 DD_TEXT_COLOR = 0.05
 
 screen = pygame.display.set_mode((VW, VH))
 clock = pygame.time.Clock()
-pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+if FULL_SCREEN: 
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 running = True
-font = pygame.font.SysFont('Arial', 48)
+font = pygame.font.SysFont('Arial', 128)
 debug_font = pygame.font.SysFont('Arial', 15)
 current_token = ""
 text_color = INIT_TEXT_COLOR
@@ -54,11 +60,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            model.write_history("context.txt")
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LSHIFT] and keys[pygame.K_9] and keys[pygame.K_SEMICOLON]:
         running = False
-        model.write_context("context.txt")
+        model.write_history("context.txt")
+    elif keys[pygame.K_ESCAPE]:
+        model.write_history("context.txt")
+        model.__init__()
+        model.seed("")
 
     # CONSUMER (very nice)
 
@@ -83,8 +94,8 @@ while running:
     text_rect.center = (VW // 2, VH // 2)
     screen.blit(text_surface, text_rect)
 
-    debug_surface = debug_font.render(str(pygame.time.get_ticks()), True, (255, 0, 255))
-    screen.blit(debug_surface, text_rect)
+    # debug_surface = debug_font.render(str(pygame.time.get_ticks()), True, (255, 0, 255))
+    # screen.blit(debug_surface, text_rect)
 
     pygame.display.flip()
 
